9caad91103e66e3a5831756a08fb3ea9b10c1234
yiwenchou <k321872001@gmail.com>
2025-12-29 21:17:59 +0800

    fix: add vite config base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,7 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  const apiKey = env.GEMINI_API_KEY || env.VITE_GEMINI_API_KEY;
-
-  return {
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-    },
-    plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(apiKey),
-      'process.env.GEMINI_API_KEY': JSON.stringify(apiKey)
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      }
-    }
-  };
-});
+export default defineConfig({
+  plugins: [react()],
+  base: '/podcast-producer/',
+})
